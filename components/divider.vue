d3ae0aa1cce6dcb6634fc0f1e3ee97ede3750bea
Thien Nguyen <tatthien.contact@gmail.com>
2022-09-19 23:31:28 +0700

    Add divider component

---
+++ components/divider.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="relative">
+		<div class="absolute inset-0 flex items-center" aria-hidden="true">
+			<div class="w-full border-t border-gray-300"></div>
+		</div>
+		<div class="relative flex justify-start">
+			<span class="bg-white pr-3 text-gray-400">{{ label }}</span>
+		</div>
+	</div>
+</template>
+
+<script lang="ts" setup>
+withDefaults(
+	defineProps<{
+		label?: string
+	}>(),
+	{
+		label: 'Divider',
+	}
+)
+</script>

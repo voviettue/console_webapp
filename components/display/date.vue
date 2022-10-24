e43e484c9e72de3ea3a48d95bd43166b1eff9cc5
Thien Nguyen <thien@catex.se>
2022-10-24 16:52:08 +0700

    Rename display components

---
+++ components/display/date.vue
@@ -0,0 +1,16 @@
+<template>
+	<span>{{ formattedDate }}</span>
+</template>
+
+<script setup lang="ts">
+import { format, parseISO, isValid } from 'date-fns'
+const props = defineProps<{
+	value: any
+}>()
+
+const formattedDate = computed(() => {
+	const date = parseISO(props.value)
+	if (!isValid(date)) return props.value
+	return format(date, 'MMM dd, yyyy H:m a')
+})
+</script>
--- plugins/display.ts
+++ plugins/display.ts
@@ -1,9 +1,11 @@
 import { Skeletor } from 'vue-skeletor'
-import DisplayBoolean from '~/components/display/display-boolean.vue'
-import DisplayStatus from '~/components/display/display-status.vue'
+import DisplayBoolean from '~/components/display/boolean.vue'
+import DisplayStatus from '~/components/display/status.vue'
+import DisplayDate from '~/components/display/date.vue'
 
 export default defineNuxtPlugin((nuxtApp) => {
 	nuxtApp.vueApp.component('display-boolean', DisplayBoolean)
 	nuxtApp.vueApp.component('display-status', DisplayStatus)
+	nuxtApp.vueApp.component('display-date', DisplayDate)
 	nuxtApp.vueApp.component(Skeletor.name, Skeletor)
 })

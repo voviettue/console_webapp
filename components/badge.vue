9a77f1856c756ace52731072a84d3dc2848b4b7a
Thien Nguyen <thien@catex.se>
2022-10-27 17:11:35 +0700

    Add variant warning to component Badge

--- components/badge.vue
+++ components/badge.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
 const props = withDefaults(
 	defineProps<{
-		variant?: 'primary' | 'secondary' | 'success' | 'danger'
+		variant?: 'primary' | 'secondary' | 'success' | 'warning' | 'danger'
 	}>(),
 	{
 		variant: 'primary',
@@ -13,6 +13,7 @@ const variantClass = computed(() => {
 	if (props.variant === 'secondary') return 'bg-gray-200 text-gray-700'
 	if (props.variant === 'success') return 'bg-green-100 text-green-800'
 	if (props.variant === 'danger') return 'bg-red-100 text-red-800'
+	if (props.variant === 'warning') return 'bg-orange-100 text-orange-800'
 	return 'bg-blue-100 text-blue-800'
 })
 </script>

1285cdd33c1f500d57ccf24efd2f7a325d6231a5
Thien Nguyen <thien@catex.se>
2022-10-27 17:07:41 +0700

    Remove unused vars

--- pages/orgs/[org-id].vue
+++ pages/orgs/[org-id].vue
@@ -24,33 +24,4 @@ store
 		showError('Organization not found')
 	})
 	.finally(() => (isFetchingOrg.value = false))
-
-const menuItems = computed(() => {
-	return [
-		{
-			name: 'Overview',
-			to: buildPath(orgId, 'overview'),
-			current: route.fullPath.startsWith(buildPath(orgId, 'overview')),
-		},
-		{
-			name: 'Databases',
-			to: buildPath(orgId, 'databases'),
-			current: route.fullPath.startsWith(buildPath(orgId, 'databases')),
-		},
-		{
-			name: 'Integrations',
-			to: buildPath(orgId, 'marketplace'),
-			current: route.fullPath.startsWith(buildPath(orgId, 'marketplace')),
-		},
-		{
-			name: 'Settings',
-			to: buildPath(orgId, 'settings'),
-			current: route.fullPath.startsWith(buildPath(orgId, 'settings')),
-		},
-	]
-})
-
-function buildPath(orgId: string, to: string) {
-	return `/orgs/${orgId}/${to}`
-}
 </script>
